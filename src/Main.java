51d319a9b2e647f62bba9dcff5ed45e3e0306dd1
Mihael Petrovic <mihael.petrovic1@hotmail.com>
2025-10-20 20:21:26 +0200

    Ispis sume brojeva.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-
+        int sum = 0;
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            sum += i;
         }
+        System.out.println("Suma brojeva je: " + sum);
     }
 }
